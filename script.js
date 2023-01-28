574b0bf2537899281eea69312c89c3a0e983d4b5
ImranShah1109 <imranshaha41@gmail.com>
2023-01-28 19:50:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+let text = document.getElementById('evaluatedText');
+        let headings = document.getElementById('wordCount');
+
+        text.addEventListener('input',()=>{
+            let str = text.value;
+            str = str.trim();
+            let arr = str.split(' ');
+            // console.log(arr.length);
+            headings.innerHTML = arr.length;
+        })
